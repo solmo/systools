ff378275110c5bfa81288de6d8c113c4c2216bc2
gustavo <gsolmoirago@gmail.com>
2015-10-27 09:34:18 -0300

    tuning admin 1

--- devices/admin.py
+++ devices/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Server,VirtualServer
 
-class VirtualServerAdmin(admin.ModelAdmin):
-	list_display = ('hostname', 'ip', 'host','backup')
 
-admin.site.register(Server)
+class VirtualServerAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'ip', 'host','backup')
+
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'ip', 'nodo')
+
+admin.site.register(Server, ServerAdmin)
 admin.site.register(VirtualServer, VirtualServerAdmin)
 
 # Register your models here.
