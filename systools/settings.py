98a9a959ef32b79f4fbb699cec5509034a7b7d32
gustavo <gsolmoirago@gmail.com>
2015-11-05 09:24:42 -0300

    inicial-celery2

--- systools/settings.py
+++ systools/settings.py
@@ -105,3 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+CELERY_TIMEZONE = TIME_ZONE
